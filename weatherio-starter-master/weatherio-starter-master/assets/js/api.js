d9b86b9f5dd51087ea8faf0c35da458101475b19
Pranavbc <pranav1952000@gmail.com>
2025-01-22 14:50:31 +0530

    commit

--- weatherio-starter-master/weatherio-starter-master/assets/js/api.js
+++ weatherio-starter-master/weatherio-starter-master/assets/js/api.js
@@ -4,3 +4,21 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+  
+  'api strict';
+
+  const api_key =  "cf6a0b63b2625078a36c4987feb5909e";
+
+  /**
+   * fetch data from server
+   * @param {string} URL  API url 
+   * @param {Function} callback  callback
+   */
+
+  export const fetchData = function(URL , callback){
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+  }
+
+  fetchData
